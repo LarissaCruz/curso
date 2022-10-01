1fab39d19b74f0926d59f9ef9b1b0ae1038b277e
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2022-10-01 11:59:30 -0300

    adding the table

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,15 +1,28 @@
 
 <x-layout title="Cursos" >
     <h1>Cursos</h1>
-    <ul class="list-group ">
-        @foreach ($cursos as $curso)
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            {{$curso->name}}
-            <form action="{{ route('cursos.index', $curso->id) }}" method="post">
-                @csrf
-                <button class="btn btn-danger btn-sm">X</button>
-            </form>
-        </li>
+    <table class="table table-striped table-dark">
+        <thead >       
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">#</th>
+            </tr>
+        </thead>
+         @foreach ($cursos as $curso)
+        <tbody>
+            <tr>
+                <th scope="row">{{$curso->id}}</th>
+                <td>{{$curso->name}}</td>
+                <td>
+                    <form action="{{ route('cursos.index', $curso->id) }}" method="post">
+                        @csrf
+                        <button class="btn btn-danger btn-sm">X</button>
+                    </form>
+                </td>
+            </tr>
+        </tbody>
         @endforeach
-    </ul>
+    </table>
+   
 </x-layout>
